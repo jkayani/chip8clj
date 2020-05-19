7a4a3138d3f89241281be08f798065401a47d826
Joshua Kayani <joshkayani@gmail.com>
2020-05-18 23:34:45 -0400

    day 2

--- src/chip8/core.clj
+++ src/chip8/core.clj
@@ -3,45 +3,48 @@
 
 (def stack-frame nil)
 
-(def vm {
-  
-  memory (byte-array 4096)
-  registers (zipmap (range 0x0 0x10) (repeat 15 0))
-  address-register 0x0000
-  stack '()
-  delay-timer nil
-  sound-timer nil
-})
+(def vm (atom {
+  "memory" (byte-array 4096)
+  "registers" (zipmap (range 0x0 0x10) (repeat 15 0))
+  "address-register" 0x0000
+  "stack" '()
+  "delay-timer" nil
+  "sound-timer" nil
+}))
 
 ; Utilities
 
 (defn read-register [register]
-  (-> (vm "registers") (register)))
+  (get-in @vm ["registers" register]))
 
-(defn update-register! [register value]
-  (let [
-      curr-registers (vm "registers")
-    ]
-    (->> 
-      (assoc curr-registers register value)
-      (assoc vm "registers")
-      (set! vm))))
+(defn update-register! [register value-fn & args]
+  (swap! vm
+    (fn [current-vm] 
+      (update-in current-vm ["registers" register] #(value-fn %)))))
 
 ; Opcodes
 
+; Writes constant to register
 (defn op6 [register constant]
-  (update-register register constant))
+  (update-register! register (constantly constant)))
 
+; Adds summand to register (no CF)
 (defn op7 [register summand]
-  (as-> 
-    (read-register register) value
-    (+ value summand)
-    (update-register! register value)))
+  (update-register! register (partial + summand)))
 
+; Writes value of reg2 to reg1
 (defn op8-assign [reg1 reg2]
-  (--> 
-    (read-register reg2) 
-    (update-register! reg1)))
+  (update-register! reg1 (constantly (read-register reg2))))
+
+(defn op8-and [reg1 reg2]
+  (update-register! reg1 (partial bit-and (read-register reg2))))
+
+(defn op8-or [reg1 reg2]
+  (update-register! reg1 (partial bit-or (read-register reg2))))
+
+(defn op8-xor [reg1 reg2]
+  (update-register! reg1 (partial bit-xor (read-register reg2))))
+ 
     
 ; Emulator execution
 
@@ -49,16 +52,19 @@
   (let [
       program (byte-array (int 1e3)) 
     ]
-    (->
-      (new java.io.FileInputStream path)
-      (.read program))))
+    (do
+      (->
+        (new java.io.FileInputStream path)
+        (.read program))
+      (swap! vm
+        #(update-in @vm ["memory"] (constantly program))))))
 
-(defn execute [program]
-  (print (vec program)))
+(defn execute []
+  (vec (@vm "memory")))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (->
+  (do
     (load-program (args 0))
     (execute)))
--- test/chip8/core_test.clj
+++ test/chip8/core_test.clj
@@ -2,6 +2,67 @@
   (:require [clojure.test :refer :all]
             [chip8.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest Emulator
+
+  (testing "execution"
+    (->> 
+      (execute (byte-array [0x61 0x01 0x61 0x02]))
+      (= [0x61 0x01 0x61 0x02])
+      (is)))
+)
+
+(deftest Opcodes
+  
+  (testing "op6: set register to constant"
+    (do
+      (op6 1 0x01)
+      (->>
+        (get-in @vm ["registers" 1])
+        (= 0x01)
+        (is))))
+
+  ; Note: Determine how to reset! an atom between test runs 
+  (testing "op7: add constant to register"
+    (do
+      (op7 1 0x01)
+      (->>
+        (get-in @vm ["registers" 1])
+        (= 0x02)
+        (is))))
+
+  (testing "op8: assign from 1 register to another"
+    (do
+      (op6 2 0xFF)
+      (op8-assign 1 2)
+      (->>
+        (get-in @vm ["registers" 1])
+        (= 0xFF)
+        (is))))
+
+  (testing "op8: AND 2 registers into the first one"
+    (do
+      (op6 2 0xFF)
+      (op8-and 1 2)
+      (->>
+        (get-in @vm ["registers" 1])
+        (= 0xFF)
+        (is))))
+
+  (testing "op8: OR 2 registers into the first one"
+    (do
+      (op6 2 0xFF)
+      (op8-or 1 2)
+      (->>
+        (get-in @vm ["registers" 1])
+        (= 0xFF)
+        (is))))
+
+  (testing "op8: XOR 2 registers into the first one"
+    (do
+      (op6 2 0xFF)
+      (op8-xor 1 2)
+      (->>
+        (get-in @vm ["registers" 1])
+        (= 0xFF)
+        (is))))
+)
